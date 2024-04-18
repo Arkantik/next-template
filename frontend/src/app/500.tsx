1de648c56c46666ae362314a330667b918e46894
Jeremy Dohin <arkantik.pro@gmail.com>
2024-04-18 20:58:58 +0200

    feat: 🎸 New error pages
    Add of 404 and 500 error template pages

---
+++ frontend/src/app/500.tsx
@@ -0,0 +1,9 @@
+export default function Error500Page() {
+	return (
+		<div className="flex h-screen items-center justify-center">
+			<h1 className="text-3xl font-semibold text-gray-800">
+				500 | Internal Server Error
+			</h1>
+		</div>
+	)
+}
---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+	return (
+		<div className="flex h-screen items-center justify-center">
+			<h1 className="text-3xl font-semibold text-gray-800">
+				404 | Page Not Found
+			</h1>
+		</div>
+	)
+}
