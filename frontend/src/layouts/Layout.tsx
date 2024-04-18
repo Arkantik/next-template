35f21e399cdc8275199fb06da9333edfcacb1409
Jeremy Dohin <arkantik.pro@gmail.com>
2024-04-18 21:04:00 +0200

    refactor: 💡 Update of layout template naming
    Changed the title of layout template & reorganized tailwind class

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,21 +1,21 @@
-import Head from "next/head";
-import { ReactNode } from "react";
+import Head from "next/head"
+import { ReactNode } from "react"
 
 interface LayoutProps {
-    children: ReactNode;
-    pageTitle?: string;
+	children: ReactNode
+	pageTitle?: string
 }
 
 export default function Layout({ children, pageTitle }: LayoutProps) {
-    return (
-        <>
-            <Head>
-                <title>The Good Corner - {pageTitle}</title>
-                <meta name="description" content="ads website" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className="main-content">{children}</main>
-        </>
-    );
+	return (
+		<>
+			<Head>
+				<title>Next Template - {pageTitle}</title>
+				<meta name="description" content="ads website" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className="main-content">{children}</main>
+		</>
+	)
 }
--- frontend/src/layouts/LayoutAdmin.tsx
+++ frontend/src/layouts/LayoutAdmin.tsx
@@ -1,22 +1,22 @@
-import Head from "next/head";
-import { ReactNode } from "react";
+import Head from "next/head"
+import { ReactNode } from "react"
 
 interface LayoutProps {
-    children: ReactNode;
-    pageTitle?: string;
+	children: ReactNode
+	pageTitle?: string
 }
 
 export default function LayoutAdmin({ children, pageTitle }: LayoutProps) {
-    return (
-        <>
-            <Head>
-                <title>The Admin Corner - {pageTitle}</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className="flex h-[100vh]">
-                <main className="p-6 overflow-y-auto">{children}</main>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<Head>
+				<title>Next Template - {pageTitle}</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="flex h-[100vh]">
+				<main className="overflow-y-auto p-6">{children}</main>
+			</div>
+		</>
+	)
 }
