1726f52086f5979dec4ea0f50fa537d8dde2e9a7
Jeremy Dohin <arkantik.pro@gmail.com>
2024-06-08 22:12:48 +0200

    feat: 🎸 Creation of env file to load all environment variables

---
+++ backend/src/env.ts
@@ -0,0 +1,14 @@
+import { load } from "ts-dotenv"
+
+export default load({
+	DB_HOST: String,
+	DB_PORT: Number,
+	DB_USER: String,
+	DB_PASS: String,
+	DB_NAME: String,
+	SERVER_PORT: Number,
+	JWT_PRIVATE_KEY: String,
+	CORS_ALLOWED_ORIGINS: String,
+	NODE_ENV: String,
+	FRONTEND_URL: String,
+})
